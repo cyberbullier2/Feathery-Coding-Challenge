8ddbed5c8a17e68a2f9db8a85ff246259091e6e8
cyberbullier2 <richardtopaloglu@gmail.com>
2024-01-13 18:51:44 -0800

    routing setup for ai app

--- urls.py
+++ urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("ai/", include("ai.urls")),
+    path("admin/", admin.site.urls),
+]

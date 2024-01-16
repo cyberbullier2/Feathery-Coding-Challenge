1a6a45229540b8b7e0a74444ffda73b9f483ae55
cyberbullier2 <richardtopaloglu@gmail.com>
2024-01-15 17:21:51 -0800

    only allow pdf file uploads

--- ai/forms.py
+++ ai/forms.py
@@ -1,4 +1,11 @@
+from django.core.exceptions import ValidationError
 from django import forms
 
 class PDFUploadForm(forms.Form):
     pdf_file = forms.FileField()
+
+    def clean_pdf_file(self):
+        file = self.cleaned_data['pdf_file']
+        if not file.name.endswith('.pdf'):
+            raise ValidationError("Only PDF files are allowed.")
+        return file
